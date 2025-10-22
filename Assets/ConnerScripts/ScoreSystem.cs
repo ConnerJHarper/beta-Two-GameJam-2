5a0fd62ecbf8666338a2d3e5a359ab4bd848b94f
ConnerJHarper <49066313+ConnerJHarper@users.noreply.github.com>
2025-10-22 14:20:07 +0100

    Added animation variable to the scoretext component so an animation will play when combo is activated
    Added animation variable to the scoretext component so an animation will play when combo is activated

--- Assets/ConnerScripts/ScoreSystem.cs
+++ Assets/ConnerScripts/ScoreSystem.cs
@@ -1,14 +1,48 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreSystem : MonoBehaviour
 {
     public int score;
     public Text scoreText;
+    public Text TimeLimit;
+
+    public Animation comboAnim;
+
+    public void Start()
+    {
+        scoreText = GetComponent<Text>();
+
+        comboAnim = GetComponent<Animation>();  
+
+        foreach (AnimationState state in comboAnim)
+        {
+            state.speed = 0.5f;
+        }
+    }
 
     public void AddTenPoints()
     {
         score += 10;
         scoreText.text = "Score" + score;
+    }
+
+    private void Update()
+    {
+        if (score == 50)
+        {
+            ComboBoost();
+        }
+
+    }
+
+
+
+    public void ComboBoost()
+    {
+        score += 30;  
+        comboAnim.Play();
     }
 }
