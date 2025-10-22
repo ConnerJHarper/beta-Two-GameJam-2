10b8e59b3c3d4714debf922264d8dcc948a4b0ce
ConnerJHarper <49066313+ConnerJHarper@users.noreply.github.com>
2025-10-22 12:19:26 +0100

    Added base line score system

--- Assets/ConnerScripts/Hook.cs
+++ Assets/ConnerScripts/Hook.cs
@@ -1,13 +1,28 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
+
+    GameObject fishCaught;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Fish"))
         {
             Debug.Log("Fish Caught!");
             Destroy(collision.gameObject);
+            fishCaught.GetComponent<ScoreSystem>().AddTenPoints();
+
+
+        }
+
+        void EndState()
+        {
+            if (score = )
+            {
+                
+            }
         }
     }
 }
